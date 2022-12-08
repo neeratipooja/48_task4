9913f3c2be7595ad4a92f4631aa2075f52c65159
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-12-08 14:54:36 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,18 @@
+"""
+DAY:thursday
+DATE:8th dec 2022
+TOPIC:task
+AUTHOR:kiranmai
+"""
+#not to display continue
+n=0
+while(n<10):
+    n+=1
+    if n==6:
+        continue
+    print(n) #1 to 10
+  #retrieve only negatives
+n=[5,10,15,20,-4,-3,-1,0,8]
+for i in n:
+    if(i<0):
+        print(i)  #-4 -3 -1
